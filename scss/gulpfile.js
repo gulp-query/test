32ab4ba9997327091eddd2d9ba2d21ec3efd537b
Marat Nuriev <nurieff@gmail.com>
2017-08-26 01:03:29 +0500

    test gulp-query-scss-images

--- scss/gulpfile.js
+++ scss/gulpfile.js
@@ -1,8 +1,10 @@
 let build = require('gulp-query');
-let scss = require('gulp-query-scss');
+//let scss = require('gulp-query-scss');
+let scss = require('gulp-query-scss-images');
 
 build((query) => {
   query.plugins([scss])
-    .scss('scss/app.scss','css/','app')
-    .scss('scss/app.scss','css/notapp.css')
+    // .scss('scss/app.scss','css/','app')
+    // .scss('scss/app.scss','css/notapp.css')
+    .scss('scss/images.scss','css/img.css')
 });
